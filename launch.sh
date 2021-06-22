f6c1ff5c050312d7f41834cb955c22937869dce0
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-06-22 18:00:15 +0530

    refactoring

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/sh -l
 nohup java -jar task.jar
--- src/main/java/in/devstream/cicd/task/CheckmarxScanTask.java
+++ src/main/java/in/devstream/cicd/task/CheckmarxScanTask.java
@@ -16,16 +16,16 @@ public class CheckmarxScanTask extends PipelineTask {
     @Override
     public void execute() {
 
-        log.info("executing the inspect task");
+        log.info("Executing checkmarx task");
     }
 
     @Override
     public void onInit() {
-        log.info("overriding on init!");
+        log.info("Initializing task...");
     }
 
     @Override
     public void onFinalize() {
-        log.info("overriding on finalize");
+        log.info("Cleaning up resources...");
     }
 }
